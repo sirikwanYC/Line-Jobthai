46cf108e74d6e78d1f493d61c00d3d5c002084f3
sirikwan <sirikwan_yanchan@hotmail.com>
2018-06-22 13:57:02 +0700

    test

--- server.js
+++ server.js
@@ -90,13 +90,11 @@ function handleEvent(event) {
       //   'type': 'text',
       //   'text': 'line://app/1589205932-WXbBEMXB'
       // })
+    default:
+      const echo = { type: 'text', text: event.message.text };
+      client.replyMessage(event.replyToken, echo);
+      break
   }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
 }
 
 // listen on port
