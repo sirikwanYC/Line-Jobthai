4dfe3c47e0754ae2758f79e2280b6e0c544d652d
chonlatit <chonlatit@thinknet.co.th>
2018-06-22 14:54:08 +0700

    no message

--- server.js
+++ server.js
@@ -74,29 +74,31 @@ function handleEvent(event) {
     case 'เลือกตำแหน่งงาน' :
       return client.replyMessage(event.replyToken, 
         {
-          type: 'template',
-          altText: 'Buttons alt text',
-          template: {
-            type: 'carousel',
-            columns: [
+          "type": "bubble",
+          "header": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
               {
-                title: 'เชฟ',
-                text: 'โรงแรมหนองป่าหอย ต้องการยอดฝีมือนักทำอาหาร ด่วน! ตำแหน่งมีจำกัด',
-                actions: [
-                  { label: 'ดูรายละเอียด', type: 'uri', uri: 'line://app/1589205932-WXbBEMXB' },
-                  { label: 'สมัครเลย', type: 'message', text: 'สมัครงาน ตำแหน่ง เชฟ' },
-                ],
-              },
-              {
-                title: 'คนทอดหมู',
-                text: 'หมูปิ้งป้าน้อย ตำบลสุเทพ อ.เมือง จ.เชียงใหม่',
-                actions: [
-                  { label: 'ดูรายละเอียด', type: 'uri', uri: 'line://app/1589205932-WXbBEMXB' },
-                  { label: 'สมัครเลย', type: 'message', text: 'สมัครงาน ตำแหน่ง คนทอดหมู' },
-                ],
-              },
+                "type": "text",
+                "text": "เลือกประเภทงานโดนใจ",
+                "weight": "bold",
+                "color": "#aaaaaa",
+                "size": "lg"
+              }
             ]
           },
+          "hero": {
+            "type": "image",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_4_news.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+              "type": "uri",
+              "uri": "http://linecorp.com/"
+            }
+          }
         }
       )
     default:
